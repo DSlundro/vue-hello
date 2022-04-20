204343207085f88ca63f8b600f236459af2e42c3
DSlundro <dragan.savic95@outlook.it>
2022-04-20 14:52:27 +0200

    commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const app = new Vue({
+    el : "#root",
+    data : {
+        welcome : {
+            message : "Lasciate ogni speranza voi ch'entrate",
+            img : "https://picsum.photos/600/400"
+        }
+    }
+})
